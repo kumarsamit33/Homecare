3d093737a57cca247a7649f610b8c4edf904137e
unknown <meen@gmail.com>
2024-11-14 16:55:41 +0530

    -meen-login

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { RouterModule, Routes } from '@angular/router';
 
 
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class LoginModule { }
